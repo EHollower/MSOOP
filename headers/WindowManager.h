cd9a45fdd04359888f19d52e2fb8b826140639b2
EHollower <76710132+EHollower@users.noreply.github.com>
2023-05-20 15:46:42 +0300

    Restructured WindowManger as a Singleton Class

---
+++ headers/WindowManager.h
@@ -0,0 +1,58 @@
+//
+// Created by Mihnea on 5/14/2023.
+//
+
+#ifndef OOP_WINDOWMANAGER_H
+#define OOP_WINDOWMANAGER_H
+
+#include <SFML/Graphics.hpp>
+#include "GameTextures.h"
+#include <iostream>
+#include <vector>
+
+class WindowManager {
+private:
+    /* constructors and destructors (private) */
+    WindowManager();
+    ~WindowManager();
+//    void create_window();
+//    void close_window();
+
+    sf::RenderWindow *app;
+    sf::Vector2f smileyStart;
+    sf::Vector2f start, dimSprite, scaleFactor, padding;
+    sf::Sprite bar1, bar2, tShape1, tShape2;
+    std::vector <sf::Sprite> corners;
+public:
+    void create_window();
+    void close_window();
+    /* instance */
+    static WindowManager* getInstance();
+
+    /* utility */
+    sf::RenderWindow* getWindow();
+    [[nodiscard]] bool isRunning() const;
+    void setView();
+    void setView(const sf::View& view);
+    sf::Vector2i get_coord();
+    void clear_window(const sf::Color& color = sf::Color(192, 192, 192));
+    void setWindowSize(const sf::Vector2u& winSize);
+    void renderView();
+    void renderSize();
+
+    /* getters */
+    sf::Vector2i getMouse() const;
+    sf::Vector2f getStart() const;
+    sf::Vector2f getSpriteDim() const;
+    sf::Vector2f getPadding() const;
+    sf::Vector2f getScaleFactor() const;
+    sf::Vector2f getSmileystart() const;
+
+    /* SFML */
+    void draw_layout();
+
+    /* operators */
+    friend std::ostream& operator << (std::ostream& stream, const WindowManager& window);
+};
+
+#endif //OOP_WINDOWMANAGER_H
---
+++ src/WindowManager.cpp
@@ -0,0 +1,174 @@
+//
+// Created by Mihnea on 5/14/2023.
+//
+
+#include "../headers/WindowManager.h"
+
+/* constructors and destructors */
+WindowManager::WindowManager(): app(nullptr),
+bar1(GameTextures::getInstance()-> getBar1()), bar2(GameTextures::getInstance()-> getBar2()),
+tShape1(GameTextures::getInstance()-> getTShape1()), tShape2(GameTextures::getInstance()-> getTShape2()) {
+    start = sf::Vector2f(20.f, 100.f);
+    dimSprite = sf::Vector2f(25.f, 25.f);
+    scaleFactor = sf::Vector2f(2.5f, 2.5f);
+    padding = sf::Vector2f(15.f, 15.f);
+    smileyStart = sf::Vector2f(0.f, 18.f); // first value does not matter
+
+    /* corners */
+    corners.resize(4);
+    corners[0].setTexture(GameTextures::getInstance()-> getCorner1());
+    corners[0].setScale(2.f, 2.f);
+    corners[1].setTexture(GameTextures::getInstance()-> getCorner2());
+    corners[1].setScale(2.f, 2.f);
+    corners[2].setTexture(GameTextures::getInstance()-> getCorner3());
+    corners[2].setScale(2.f, 2.f);
+    corners[3].setTexture(GameTextures::getInstance()-> getCorner4());
+    corners[3].setScale(2.f, 2.f);
+    create_window();
+}
+
+WindowManager::~WindowManager() {
+    close_window();
+    delete app;
+}
+
+void WindowManager::create_window() {
+    if (app == nullptr) {
+        app = new sf::RenderWindow(sf::VideoMode({400, 480}), "Minesweeper", sf::Style::Titlebar | sf::Style::Close);
+
+        sf::Image icon;
+        icon.loadFromFile("assets/icon.png");
+        app->setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+        app->setFramerateLimit(60);
+    }
+}
+
+void WindowManager::close_window() {
+    app-> close();
+}
+
+WindowManager* WindowManager::getInstance() {
+    static WindowManager* instance = nullptr;
+    if (!instance) {
+        instance = new WindowManager();
+    }
+    return instance;
+}
+
+sf::RenderWindow* WindowManager::getWindow() {
+    return app;
+}
+
+bool WindowManager::isRunning() const {
+    return app-> isOpen();
+}
+
+void WindowManager::clear_window(const sf::Color& color) {
+    app-> clear(color);
+}
+
+void WindowManager::setWindowSize(const sf::Vector2u& winSize) {
+    app->setSize(winSize);
+}
+
+void WindowManager::setView() {
+     app-> setView(sf::View(sf::FloatRect(0, 0,
+     static_cast<float>(app-> getSize().x), static_cast <float>(app-> getSize().y))));
+}
+
+void WindowManager::setView(const sf::View& view) {
+    app-> setView(view);
+}
+
+void WindowManager::renderView() {
+    app-> display();
+}
+
+sf::Vector2i WindowManager::get_coord() {
+    sf::Vector2f mousePos = app-> mapPixelToCoords(sf::Mouse::getPosition(*app));
+    int i = static_cast<int>((mousePos.x - start.x) / (dimSprite.x + padding.x));
+    int j = static_cast<int>((mousePos.y - start.y) / (dimSprite.y + padding.y));
+
+    float cellX = start.x + static_cast<float>(i) * (dimSprite.x + padding.x);
+    float cellY = start.y + static_cast<float>(j) * (dimSprite.y + padding.y);
+    float cellWidth = dimSprite.x * scaleFactor.x;
+    float cellHeight = dimSprite.y * scaleFactor.y;
+
+    if (mousePos.x >= cellX && mousePos.x <= (cellX + cellWidth) &&
+        mousePos.y >= cellY && mousePos.y <= (cellY + cellHeight)) {
+        return {i, j};
+    }
+
+    return {-1, -1};
+}
+
+void WindowManager::renderSize() {
+    smileyStart.x = 0.35f * static_cast<float>(app-> getSize().x);
+}
+
+sf::Vector2i WindowManager::getMouse() const {
+    return sf::Mouse::getPosition(*app);
+}
+
+sf::Vector2f WindowManager::getStart() const {
+    return start;
+}
+
+sf::Vector2f WindowManager::getSmileystart() const {
+    return smileyStart;
+}
+
+sf::Vector2f WindowManager::getSpriteDim() const {
+    return dimSprite;
+}
+
+sf::Vector2f WindowManager::getPadding() const {
+    return padding;
+}
+
+sf::Vector2f WindowManager::getScaleFactor() const {
+    return scaleFactor;
+}
+
+void WindowManager::draw_layout() {
+    /* first set of bars */
+    bar1.setPosition(0, 0);
+    bar1.setScale(static_cast<float>(app-> getSize().x), 2.f);
+    app-> draw(bar1);
+    bar1.setPosition(0, 83.f);
+    app->draw(bar1);
+    bar1.setPosition(0, static_cast<float>(app-> getSize().y) - 20.f);
+    app->draw(bar1);
+
+    /* second set of bars */
+    bar2.setTexture(GameTextures::getInstance()->getBar2());
+    bar2.setPosition(0.f, 0.f);
+    bar2.setScale(2.f, static_cast<float>(app-> getSize().y));
+    app->draw(bar2);
+    bar2.setPosition(static_cast<float>(app-> getSize().x) - 20.f, 0.f);
+    app->draw(bar2);
+
+    /* tShapes */
+    tShape1.setPosition(0, 83.f);
+    tShape1.setScale(2.f, 2.f);
+    app-> draw(tShape1);
+    tShape2.setPosition(static_cast<float>(app-> getSize().x) - 20.f, 83.f);
+    tShape2.setScale(2.f, 2.f);
+    app-> draw(tShape2);
+
+    /* corners */
+    corners[0].setPosition(0, 0);
+    app-> draw(corners[0]);
+    corners[1].setPosition(static_cast<float>(app-> getSize().x) - 20.f, 0.f);
+    app-> draw(corners[1]);
+    corners[2].setPosition(0, static_cast<float>(app-> getSize().y) - 20.f);
+    app-> draw(corners[2]);
+    corners[3].setPosition(static_cast<float>(app-> getSize().x) - 20.f,
+                           static_cast<float>(app-> getSize().y) - 20.f);
+    app-> draw(corners[3]);
+}
+
+std::ostream& operator << (std::ostream& stream, const WindowManager& window) {
+    stream << window.app-> getSize().x << " " << window.app-> getSize().y << "\n";
+    return stream;
+}
