15210be4b1ab7dda5fc069b0d8b2a444d938b851
EHollower <76710132+EHollower@users.noreply.github.com>
2023-05-20 15:43:52 +0300

    Game class for Game Menu

---
+++ headers/Game.h
@@ -0,0 +1,29 @@
+//
+// Created by Mihnea on 5/14/2023.
+//
+
+#ifndef OOP_GAME_H
+#define OOP_GAME_H
+
+#include <SFML/Graphics.hpp>
+#include "Application.h"
+#include "GameBoard.h"
+#include "Smiley.h"
+#include "Entity.h"
+#include <iostream>
+
+class Game: public Entity {
+private:
+    std::shared_ptr<GameBoard> board;
+    Smiley smiley;
+    sf::View view;
+    int row, col, nr;
+public:
+    Game(int _row, int _col, int _nr);
+    ~Game() override = default;
+    void draw() override;
+    void update() override;
+    void checkIfEnded();
+};
+
+#endif //OOP_GAME_H
---
+++ src/Game.cpp
@@ -0,0 +1,90 @@
+//
+// Created by Mihnea on 5/14/2023.
+//
+
+#include "../headers/Game.h"
+
+Game::Game(int _row, int _col, int _nr) : row(_row), col(_col), nr(_nr) {
+    board = std::make_shared<GameBoard>(row, col, nr);
+    board-> scaleBoard(windowManager->getStart(), windowManager->getSpriteDim(),
+                       windowManager-> getScaleFactor(), windowManager->getPadding());
+}
+
+void Game::checkIfEnded() {
+    if (!board-> isRunning() or board-> onlyMines()) {
+        board-> reveal();
+        if (board-> status() == -1) smiley.setLoss();
+        else smiley.setWin();
+    }
+
+}
+
+void Game::draw() {
+    checkIfEnded();
+    windowManager->setView();
+    board-> scaleBoard(windowManager->getStart(), windowManager->getSpriteDim(),
+                       windowManager-> getScaleFactor(), windowManager->getPadding());
+    windowManager->clear_window();
+    windowManager->draw_layout();
+    board-> drawBoard(*windowManager->getWindow());
+    smiley.drawSmiley(*windowManager->getWindow());
+    windowManager->renderView();
+}
+
+void Game::update() {
+    sf::Event event{};
+    while (windowManager-> getWindow()-> pollEvent(event)) {
+        switch(event.type) {
+            case sf::Event::Closed: {
+                windowManager-> getWindow()->close();
+                break;
+            }
+            case sf::Event::MouseButtonPressed: {
+                if (event.mouseButton.button == sf::Mouse::Right) {
+                    if (board->status() == 0)
+                        board->putFlag(windowManager->get_coord().x, windowManager->get_coord().y);
+                }
+
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    if (smiley.containsSmiley(windowManager->getMouse())) {
+                        smiley.pressCellSmiley();
+                        smiley.setSmiley();
+                        draw(); sf::sleep(sf::milliseconds(5));
+                        smiley.pressCellSmiley(); board.reset();
+                        board = std::make_unique <GameBoard>(row, col, nr);
+                        board-> scaleBoard(windowManager->getStart(), windowManager->getSpriteDim(),
+                                           windowManager-> getScaleFactor(), windowManager->getPadding());
+                    }
+
+                    if (smiley.containsSettings(windowManager->getMouse())) {
+                        smiley.pressCellSettings();
+                        smiley.setSmiley();
+                        draw(); sf::sleep(sf::milliseconds(5));
+                        smiley.pressCellSettings();
+                        return;
+                    }
+
+                    while (sf::Mouse::isButtonPressed(sf::Mouse::Left) and board->status() == 0) {
+                        if (board-> getCell(windowManager->get_coord().x, windowManager->get_coord().y).isCovered() and
+                           !board-> getCell(windowManager->get_coord().x, windowManager->get_coord().y).isFlag()) {
+                            smiley.setIDK();
+                        }
+                        board->pressCells(windowManager->get_coord().x, windowManager->get_coord().y);
+                        draw();
+                        smiley.setSmiley();
+                    }
+                }
+                break;
+            }
+            case sf::Event::MouseButtonReleased: {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    board->setUnpressed();
+                    board->touch(windowManager->get_coord().x, windowManager->get_coord().y);
+                }
+                break;
+            }
+            default:
+                break;
+        }
+    }
+}
